8fd15ce505e3c3d7840f71456aca1bf3150651a7
Herminio <herminiopaucar@gmail.com>
2025-11-12 22:21:55 -0500

    upload new code

--- laboratory/OpenMP/5-ImageProcess/principal2.c
+++ laboratory/OpenMP/5-ImageProcess/principal2.c
@@ -2,163 +2,194 @@
  * Para probar con la versión en paralelo con N hilos
  * debes definir la constante durante la compilación
  * gracias a la opción -D:
- *  gcc principal.c -o test -DPARALLEL = N -fopenmp -Wall
- *  Ex:
- *  gcc principal1.c -o test -fopenmp -Wall -DPARALLEL=100
+ *
+ *   gcc principal.c -o test -DPARALLEL=N -fopenmp -Wall
+ *   Ejemplo:
+ *   gcc principal1.c -o test -fopenmp -Wall -DPARALLEL=100
  ********************************************************/
 
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <string.h>
-
 #include <assert.h>
-
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define NB_PIXELS 256
 
 typedef struct color_pixel_struct {
     unsigned char r, g, b;
-}
-color_pixel_type;
+} color_pixel_type;
 
 typedef struct color_image_struct {
     int width, height;
-    color_pixel_type * pixels;
-}
-color_image_type;
+    color_pixel_type *pixels;
+} color_image_type;
 
 typedef struct grey_image_struct {
     int width, height;
-    unsigned char * pixels;
-}
-grey_image_type;
+    unsigned char *pixels;
+} grey_image_type;
 
 /**********************************************************************/
-
-color_image_type * loadColorImage(char * filename) {
+color_image_type *loadColorImage(char *filename) {
     int i, width, height, max_value;
     char format[8];
-    color_image_type * image;
-    FILE * f = fopen(filename, "r");
+    color_image_type *image;
+
+    FILE *f = fopen(filename, "r");
     if (!f) {
         fprintf(stderr, "Cannot open file %s...\n", filename);
         exit(-1);
     }
+
     fscanf(f, "%s\n", format);
-    assert((format[0] == 'P' && format[1] == '3')); // check P3 format
-    while (fgetc(f) == '#') // commentaire
-    {
-        while (fgetc(f) != '\n'); // termina la ejecucion
+    assert((format[0] == 'P' && format[1] == '3'));  // check P3 format
+
+    // Saltar comentarios
+    while (fgetc(f) == '#') {
+        while (fgetc(f) != '\n') {
+            // ir al final de la línea
+        }
     }
     fseek(f, -1, SEEK_CUR);
-    fscanf(f, "%d %d\n", & width, & height);
-    fscanf(f, "%d\n", & max_value);
+
+    fscanf(f, "%d %d\n", &width, &height);
+    fscanf(f, "%d\n", &max_value);
+
     image = malloc(sizeof(color_image_type));
     assert(image != NULL);
-    image -> width = width;
-    image -> height = height;
-    image -> pixels = malloc(width * height * sizeof(color_pixel_type));
-    assert(image -> pixels != NULL);
+
+    image->width = width;
+    image->height = height;
+    image->pixels = malloc(width * height * sizeof(color_pixel_type));
+    assert(image->pixels != NULL);
 
     for (i = 0; i < width * height; i++) {
         int r, g, b;
-        fscanf(f, "%d %d %d", & r, & g, & b);
-        image -> pixels[i].r = (unsigned char) r;
-        image -> pixels[i].g = (unsigned char) g;
-        image -> pixels[i].b = (unsigned char) b;
+        fscanf(f, "%d %d %d", &r, &g, &b);
+        image->pixels[i].r = (unsigned char)r;
+        image->pixels[i].g = (unsigned char)g;
+        image->pixels[i].b = (unsigned char)b;
     }
+
     fclose(f);
     return image;
 }
 
 /**********************************************************************/
-grey_image_type * createGreyImage(int width, int height) {
-    grey_image_type * image = malloc(sizeof(grey_image_type));
+grey_image_type *createGreyImage(int width, int height) {
+    grey_image_type *image = malloc(sizeof(grey_image_type));
     assert(image != NULL);
-    image -> width = width;
-    image -> height = height;
-    image -> pixels = malloc(width * height * sizeof(unsigned char));
-    assert(image -> pixels != NULL);
-    return (image);
+
+    image->width = width;
+    image->height = height;
+    image->pixels = malloc(width * height * sizeof(unsigned char));
+    assert(image->pixels != NULL);
+
+    return image;
 }
 
 /**********************************************************************/
-void saveGreyImage(char * filename, grey_image_type * image) {
+void saveGreyImage(char *filename, grey_image_type *image) {
     int i;
-    FILE * f = fopen(filename, "w");
+    FILE *f = fopen(filename, "w");
     if (!f) {
         fprintf(stderr, "Cannot open file %s...\n", filename);
         exit(-1);
     }
-    fprintf(f, "P2\n%d %d\n255\n", image -> width, image -> height);
-    for (i = 0; i < image -> width * image -> height; i++) {
-        fprintf(f, "%d\n", image -> pixels[i]);
+
+    fprintf(f, "P2\n%d %d\n255\n", image->width, image->height);
+
+    for (i = 0; i < image->width * image->height; i++) {
+        fprintf(f, "%d\n", image->pixels[i]);
     }
+
     fclose(f);
 }
 
 /*******************************************************
  * Transforms a color image PPM to grey PGM
  ********************************************************/
-grey_image_type * colorImageToGrey(color_image_type * colorImage) {
+grey_image_type *colorImageToGrey(color_image_type *colorImage) {
     double t, start, stop;
     start = omp_get_wtime();
-    int width = colorImage -> width, height = colorImage -> height;
-    grey_image_type * greyImage = createGreyImage(width, height);
+
+    int width = colorImage->width;
+    int height = colorImage->height;
+    grey_image_type *greyImage = createGreyImage(width, height);
+
     int r, g, b;
-    #ifdef PARALLEL
-    #pragma omp parallel
-    for private(r, g, b) num_threads(PARALLEL)
-    #endif
+
+#ifdef PARALLEL
+#pragma omp parallel for private(r, g, b) num_threads(PARALLEL)
+#endif
     for (int i = 0; i < width * height; i++) {
-        r = colorImage -> pixels[i].r;
-        g = colorImage -> pixels[i].g;
-        b = colorImage -> pixels[i].b;
-        greyImage -> pixels[i] = (299 * r + 587 * g + 114 * b) / 1000;
+        r = colorImage->pixels[i].r;
+        g = colorImage->pixels[i].g;
+        b = colorImage->pixels[i].b;
+        greyImage->pixels[i] = (299 * r + 587 * g + 114 * b) / 1000;
     }
+
     stop = omp_get_wtime();
     t = stop - start;
     printf("TEMPS-colorToGrey: %lf\n", t);
+
     return greyImage;
 }
-// Una pequeña función que calcula el relieve de píxeles
-char greyPixelToColor(unsigned char * pixels, int index, int width, int height) {
-    return 128 - 2 * pixels[index - width - 1] - pixels[index - width] - pixels[index - 1] +
-        pixels[index + 1] + pixels[index + width] + 2 * pixels[index + width + 1];
 
+/*******************************************************
+ * Una pequeña función que calcula el relieve de píxeles
+ *******************************************************/
+char greyPixelToColor(unsigned char *pixels, int index, int width, int height) {
+    return 128
+           - 2 * pixels[index - width - 1]
+           - pixels[index - width]
+           - pixels[index - 1]
+           + pixels[index + 1]
+           + pixels[index + width]
+           + 2 * pixels[index + width + 1];
 }
+
 /*******************************************************
  * La función de estampado
  *******************************************************/
-grey_image_type * greyImageToColor(grey_image_type * greyImage) {
+grey_image_type *greyImageToColor(grey_image_type *greyImage) {
     double t, start, stop;
     start = omp_get_wtime();
-    int width = greyImage -> width, height = greyImage -> height;
-    grey_image_type * colorImage = createGreyImage(width, height);
-    #ifdef PARALLEL
-    #pragma omp parallel
-    for num_threads(PARALLEL)
-    #endif
+
+    int width = greyImage->width;
+    int height = greyImage->height;
+    grey_image_type *colorImage = createGreyImage(width, height);
+
+#ifdef PARALLEL
+#pragma omp parallel for num_threads(PARALLEL)
+#endif
     for (int i = 0; i < width * height; i++) {
         if (i < width || i % width == 0 || (i + 1) % width == 0 || (height - 1) * width <= i) {
-            colorImage -> pixels[i] = greyImage -> pixels[i];
+            colorImage->pixels[i] = greyImage->pixels[i];
         } else {
-            colorImage -> pixels[i] = greyPixelToColor(greyImage -> pixels, i, width, height);
+            colorImage->pixels[i] =
+                greyPixelToColor(greyImage->pixels, i, width, height);
         }
     }
+
     stop = omp_get_wtime();
     t = stop - start;
     printf("TEMPS-greyTocolor: %lf\n", t);
+
     return colorImage;
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
+    saveGreyImage(
+        "src/resize/image0_color.pgm",
+        greyImageToColor(colorImageToGrey(loadColorImage("src/image0.ppm")))
+    );
 
-    saveGreyImage("src/resize/image0_color.pgm", greyImageToColor(colorImageToGrey(loadColorImage("src/image0.ppm"))));
-    saveGreyImage("src/resize/image2_color.pgm", greyImageToColor(colorImageToGrey(loadColorImage("src/image2.ppm"))));
+    saveGreyImage(
+        "src/resize/image2_color.pgm",
+        greyImageToColor(colorImageToGrey(loadColorImage("src/image2.ppm")))
+    );
 
     return 0;
 }
--- laboratory/OpenMP/5-ImageProcess/principal3.c
+++ laboratory/OpenMP/5-ImageProcess/principal3.c
@@ -2,190 +2,216 @@
  * Para probar con la versión en paralelo con N hilos
  * debes definir la constante durante la compilación
  * gracias a la opción -D:
- *  gcc principal.c -o test -DPARALLEL = N -fopenmp -Wall
- *  Ex:
- *  gcc principal1.c -o test -fopenmp -Wall -DPARALLEL=100
+ *
+ *   gcc principal.c -o test -DPARALLEL=N -fopenmp -Wall
+ *   Ejemplo:
+ *   gcc principal1.c -o test -fopenmp -Wall -DPARALLEL=100
  ********************************************************/
 
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <string.h>
-
 #include <assert.h>
-
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define NB_PIXELS 256
 
 typedef struct color_pixel_struct {
     unsigned char r, g, b;
-}
-color_pixel_type;
+} color_pixel_type;
 
 typedef struct color_image_struct {
     int width, height;
-    color_pixel_type * pixels;
-}
-color_image_type;
+    color_pixel_type *pixels;
+} color_image_type;
 
 typedef struct grey_image_struct {
     int width, height;
-    unsigned char * pixels;
-}
-grey_image_type;
+    unsigned char *pixels;
+} grey_image_type;
 
 /**********************************************************************/
-
-color_image_type * loadColorImage(char * filename) {
+color_image_type *loadColorImage(char *filename) {
     int i, width, height, max_value;
     char format[8];
-    color_image_type * image;
-    FILE * f = fopen(filename, "r");
+    color_image_type *image;
+
+    FILE *f = fopen(filename, "r");
     if (!f) {
         fprintf(stderr, "Cannot open file %s...\n", filename);
         exit(-1);
     }
+
     fscanf(f, "%s\n", format);
-    assert((format[0] == 'P' && format[1] == '3')); // check P3 format
-    while (fgetc(f) == '#') // commentaire
-    {
-        while (fgetc(f) != '\n'); // aller jusqu'a la fin de la ligne
+    assert((format[0] == 'P' && format[1] == '3'));  // check P3 format
+
+    // Saltar comentarios
+    while (fgetc(f) == '#') {
+        while (fgetc(f) != '\n') {
+            // ir hasta el final de la línea
+        }
     }
     fseek(f, -1, SEEK_CUR);
-    fscanf(f, "%d %d\n", & width, & height);
-    fscanf(f, "%d\n", & max_value);
+
+    fscanf(f, "%d %d\n", &width, &height);
+    fscanf(f, "%d\n", &max_value);
+
     image = malloc(sizeof(color_image_type));
     assert(image != NULL);
-    image -> width = width;
-    image -> height = height;
-    image -> pixels = malloc(width * height * sizeof(color_pixel_type));
-    assert(image -> pixels != NULL);
+
+    image->width = width;
+    image->height = height;
+    image->pixels = malloc(width * height * sizeof(color_pixel_type));
+    assert(image->pixels != NULL);
 
     for (i = 0; i < width * height; i++) {
         int r, g, b;
-        fscanf(f, "%d %d %d", & r, & g, & b);
-        image -> pixels[i].r = (unsigned char) r;
-        image -> pixels[i].g = (unsigned char) g;
-        image -> pixels[i].b = (unsigned char) b;
+        fscanf(f, "%d %d %d", &r, &g, &b);
+        image->pixels[i].r = (unsigned char)r;
+        image->pixels[i].g = (unsigned char)g;
+        image->pixels[i].b = (unsigned char)b;
     }
+
     fclose(f);
     return image;
 }
 
 /**********************************************************************/
-
-grey_image_type * createGreyImage(int width, int height) {
-    grey_image_type * image = malloc(sizeof(grey_image_type));
+grey_image_type *createGreyImage(int width, int height) {
+    grey_image_type *image = malloc(sizeof(grey_image_type));
     assert(image != NULL);
-    image -> width = width;
-    image -> height = height;
-    image -> pixels = malloc(width * height * sizeof(unsigned char));
-    assert(image -> pixels != NULL);
-    return (image);
+
+    image->width = width;
+    image->height = height;
+    image->pixels = malloc(width * height * sizeof(unsigned char));
+    assert(image->pixels != NULL);
+
+    return image;
 }
 
 /**********************************************************************/
-
-void saveGreyImage(char * filename, grey_image_type * image) {
+void saveGreyImage(char *filename, grey_image_type *image) {
     int i;
-    FILE * f = fopen(filename, "w");
+    FILE *f = fopen(filename, "w");
     if (!f) {
         fprintf(stderr, "Cannot open file %s...\n", filename);
         exit(-1);
     }
-    fprintf(f, "P2\n%d %d\n255\n", image -> width, image -> height);
-    for (i = 0; i < image -> width * image -> height; i++) {
-        fprintf(f, "%d\n", image -> pixels[i]);
+
+    fprintf(f, "P2\n%d %d\n255\n", image->width, image->height);
+
+    for (i = 0; i < image->width * image->height; i++) {
+        fprintf(f, "%d\n", image->pixels[i]);
     }
+
     fclose(f);
 }
 
 /*******************************************************
  * Transforms a color image PPM to grey PGM
  ********************************************************/
-grey_image_type * colorImageToGrey(color_image_type * colorImage) {
+grey_image_type *colorImageToGrey(color_image_type *colorImage) {
     double t, start, stop;
     start = omp_get_wtime();
-    int width = colorImage -> width, height = colorImage -> height;
-    grey_image_type * greyImage = createGreyImage(width, height);
+
+    int width = colorImage->width;
+    int height = colorImage->height;
+    grey_image_type *greyImage = createGreyImage(width, height);
+
     int r, g, b;
-    #ifdef PARALLEL
-    #pragma omp parallel
-    for private(r, g, b) num_threads(PARALLEL)
-    #endif
+
+#ifdef PARALLEL
+#pragma omp parallel for private(r, g, b) num_threads(PARALLEL)
+#endif
     for (int i = 0; i < width * height; i++) {
-        r = colorImage -> pixels[i].r;
-        g = colorImage -> pixels[i].g;
-        b = colorImage -> pixels[i].b;
-        greyImage -> pixels[i] = (299 * r + 587 * g + 114 * b) / 1000;
+        r = colorImage->pixels[i].r;
+        g = colorImage->pixels[i].g;
+        b = colorImage->pixels[i].b;
+        greyImage->pixels[i] = (299 * r + 587 * g + 114 * b) / 1000;
     }
+
     stop = omp_get_wtime();
     t = stop - start;
     printf("TEMPS-colorToGrey: %lf\n", t);
+
     return greyImage;
 }
 
 /*******************************************************
  * La función que aumenta el contraste de una imagen.
  *******************************************************/
-grey_image_type * transform(grey_image_type * greyImage) {
+grey_image_type *transform(grey_image_type *greyImage) {
     double t, start, stop;
     start = omp_get_wtime();
-    int width = greyImage -> width, height = greyImage -> height;
-    grey_image_type * contrastImage = createGreyImage(width, height);
-    int * H, * C;
-    H = calloc(NB_PIXELS, sizeof(int)); // PARA ASEGURARSE DE QUE LAS CAJAS DE MEMORIA RESERVADAS SE INICIAN EN CERO
+
+    int width = greyImage->width;
+    int height = greyImage->height;
+    grey_image_type *contrastImage = createGreyImage(width, height);
+
+    int *H, *C;
+    H = calloc(NB_PIXELS, sizeof(int));  // inicializar en cero
     C = calloc(NB_PIXELS, sizeof(int));
+
     unsigned char pixel = 0;
-    #ifdef PARALLEL
+
+#ifdef PARALLEL
     // Para ganar aceleración real es mejor no paralelizar
     // este bucle porque contiene una sección crítica
-    #pragma omp parallel
-    for private(pixel) num_threads(PARALLEL)
-    #endif
+#pragma omp parallel for private(pixel) num_threads(PARALLEL)
+#endif
     for (int i = 0; i < width * height; i++) {
-        pixel = greyImage -> pixels[i];
-        #ifdef PARALLEL
-        // ESTA INSTRUCCIONES DEBEN SER ATÓMICAS, DE OTRA MANERA, SE PUEDEN HACER MÚLTIPLES ACCESOS Y MODIFICACIONES AL MISMO TIEMPO
-        #pragma omp critical {
-            #endif
+        pixel = greyImage->pixels[i];
+#ifdef PARALLEL
+        // Esta instrucción debe ser atómica; de otra manera
+        // se pueden hacer múltiples accesos y modificaciones al mismo tiempo
+#pragma omp critical
+        {
+#endif
             H[pixel]++;
-            #ifdef PARALLEL
+#ifdef PARALLEL
         }
-        #endif
+#endif
     }
-    #ifdef PARALLEL
-    // PODEMOS PARALIZAR ESTE LAZO PORQUE CADA INSTRUCCIÓN ES INDEPENDIENTE EN CADA ITERACIÓN DE (i)
-    #pragma omp parallel
-    for num_threads(PARALLEL) schedule(dynamic, 50)
-    #endif
+
+#ifdef PARALLEL
+    // Podemos paralelizar este lazo porque cada instrucción
+    // es independiente en cada iteración de i
+#pragma omp parallel for num_threads(PARALLEL) schedule(dynamic, 50)
+#endif
     for (int i = 0; i < NB_PIXELS; i++) {
         for (int pixel_j = 0; pixel_j <= i; pixel_j++) {
             C[i] += H[pixel_j];
         }
     }
-    #ifdef PARALLEL
-    #pragma omp parallel
-    for private(pixel) num_threads(PARALLEL)
-    #endif
+
+#ifdef PARALLEL
+#pragma omp parallel for private(pixel) num_threads(PARALLEL)
+#endif
     for (int i = 0; i < width * height; i++) {
-        pixel = greyImage -> pixels[i];
-        contrastImage -> pixels[i] = 256 * C[pixel] / (width * height);
+        pixel = greyImage->pixels[i];
+        contrastImage->pixels[i] = 256 * C[pixel] / (width * height);
     }
+
     free(H);
     free(C);
+
     stop = omp_get_wtime();
     t = stop - start;
     printf("TEMPS-transform: %lf\n", t);
+
     return contrastImage;
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
+    saveGreyImage(
+        "src/resize/image0_contraste.pgm",
+        transform(colorImageToGrey(loadColorImage("src/image0.ppm")))
+    );
 
-    saveGreyImage("src/resize/image0_contraste.pgm", transform(colorImageToGrey(loadColorImage("src/image0.ppm"))));
-    saveGreyImage("src/resize/image2_contraste.pgm", transform(colorImageToGrey(loadColorImage("src/image2.ppm"))));
+    saveGreyImage(
+        "src/resize/image2_contraste.pgm",
+        transform(colorImageToGrey(loadColorImage("src/image2.ppm")))
+    );
 
     return 0;
 }
